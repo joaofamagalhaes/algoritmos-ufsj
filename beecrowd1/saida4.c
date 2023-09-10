05895a3f25e356ccb42bb5d66716c8b08d9964e9
joaofamagalhaes <joaomagalhaes.pro@gmail.com>
2023-09-10 14:06:51 -0300

    10/09

---
+++ beecrowd1/saida4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main (){
+
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d = 3;
+    int e = 4;
+    int f = 5;
+    int g = 6;
+    int h = 7;
+    int i = 8;
+    int j = 9;
+    int k = 10;
+    int l = 11;
+    int m = 12;
+    int n = 13;
+    int o = 14; 
+    int p = 15;
+
+    printf("---------------------------------------\n");
+    printf("|  decimal  |  octal  |  Hexadecimal  |\n");
+    printf("---------------------------------------\n");
+    printf("|      %d    |    %o    |       %X       |\n", a, a, a);
+    printf("|      %d    |    %o    |       %X       |\n", b, b, b);
+    printf("|      %d    |    %o    |       %X       |\n", c, c, c);
+    printf("|      %d    |    %o    |       %X       |\n", d, d, d);
+    printf("|      %d    |    %o    |       %X       |\n", e, e, e);
+    printf("|      %d    |    %o    |       %X       |\n", f, f, f);
+    printf("|      %d    |    %o    |       %X       |\n", g, g, g);
+    printf("|      %d    |    %o    |       %X       |\n", h, h, h);
+    printf("|      %d    |   %o    |       %X       |\n", i, i, i);
+    printf("|      %d    |   %o    |       %X       |\n", j, j, j);
+    printf("|     %d    |   %o    |       %X       |\n", k, k, k);
+    printf("|     %d    |   %o    |       %X       |\n", l, l, l);
+    printf("|     %d    |   %o    |       %X       |\n", m, m, m);
+    printf("|     %d    |   %o    |       %X       |\n", n, n, n);
+    printf("|     %d    |   %o    |       %X       |\n", o, o, o);
+    printf("|     %d    |   %o    |       %X       |\n", p, p, p);
+    printf("---------------------------------------\n");
+   return 0;
+}
