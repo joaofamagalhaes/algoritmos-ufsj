78e65eb18b8e040c9f249fe799123f6c1cde5ef2
joaofamagalhaes <joaomagalhaes.pro@gmail.com>
2023-09-11 19:24:35 -0300

    11/09

--- beecrowd1/idade_em_dias.c
+++ beecrowd1/idade_em_dias.c
@@ -4,10 +4,16 @@ int main (){
     
     int idade, ano, mes, dia;
 
-    scanf("%d", idade); 
-
+    scanf("%d", &idade); 
+    
     ano = idade / 365;
+    mes = (idade / 30) % 12;
+    dia = (idade % 365) % 30;
+    
 
+    printf("%d ano (s)\n", ano);
+    printf("%d mes (s)\n", mes);
+    printf("%d dia (s)\n", dia);
 
     return 0;
 }
